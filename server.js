09cb310e2ac410349125186c505762dabb3082a2
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-03-16 23:17:35 +0300

    setup server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome." });
+});
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
