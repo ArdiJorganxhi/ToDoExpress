9c8d69d5a3737edadac8f8ed36347473b758d103
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-03-16 23:42:52 +0300

    database config

---
+++ models/user.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    name: {
+      type: Sequelize.STRING
+    },
+    surname: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+        type: Sequelize.STRING
+    }
+  });
+
+  return User;
+};
